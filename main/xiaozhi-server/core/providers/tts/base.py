72d9f0508883cad4fc0a346327a3686ea36f15ef
QJH <qiaojianhua@zjbci.com>
2025-06-05 19:29:49 +0800

    添加参数 联通1

--- main/xiaozhi-server/core/providers/tts/base.py
+++ main/xiaozhi-server/core/providers/tts/base.py
@@ -19,6 +19,7 @@ from core.providers.tts.dto.dto import (
     SentenceType,
     ContentType,
     InterfaceType,
+    VoiceGender,
 )
 
 import traceback
@@ -395,3 +396,10 @@ class TTSProviderBase(ABC):
                 self.processed_chars += len(full_text)
                 return True
         return False
+
+    def update_config(self, voice_config: dict):
+        """更新 tts 配置信息
+        1. 语速
+        2. 声音名称、角色
+        """
+        pass
