6c52f28918444bc627fa2690f49e0a6481d2c120
QJH <qiaojianhua@zjbci.com>
2025-06-11 14:35:30 +0800

    fix: 手动模式下，不启用vad

--- main/xiaozhi-server/core/handle/receiveAudioHandle.py
+++ main/xiaozhi-server/core/handle/receiveAudioHandle.py
@@ -12,7 +12,7 @@ TAG = __name__
 
 async def handleAudioMessage(conn, audio):
     # 当前片段是否有人说话
-    have_voice = conn.vad.is_vad(conn, audio)
+    have_voice = True if conn.client_listen_mode == "manual" else conn.vad.is_vad(conn, audio)
     # 如果设备刚刚被唤醒，短暂忽略VAD检测
     if have_voice and hasattr(conn, "just_woken_up") and conn.just_woken_up:
         have_voice = False
