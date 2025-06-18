a28a85818ba4f0694576345e55de4ac22b46e3f7
QJH <qiaojianhua@zjbci.com>
2025-06-18 17:22:28 +0800

    update: tts_test 音频路径可配置

--- main/xiaozhi-server/core/providers/tts/tts_test.py
+++ main/xiaozhi-server/core/providers/tts/tts_test.py
@@ -1,3 +1,5 @@
+import os.path
+
 from core.providers.tts.base import TTSProviderBase
 from config.logger import setup_logging
 
@@ -8,11 +10,14 @@ logger = setup_logging()
 class TTSProvider(TTSProviderBase):
     def __init__(self, config, delete_audio_file):
         super().__init__(config, delete_audio_file)
+        self.audio_path = config.get("audio_path", "")
+        if not os.path.exists(self.audio_path):
+            self.audio_path = r".\config\assets\bind_code\0.wav"
 
     async def text_to_speak(self, text, output_file):
         try:
             pass
-            with open(r".\config\assets\bind_code\0.wav", "rb") as f:
+            with open(self.audio_path, "rb") as f:
                 return f.read()
         except Exception as e:
             raise Exception(f"{__name__} error: {e}")
